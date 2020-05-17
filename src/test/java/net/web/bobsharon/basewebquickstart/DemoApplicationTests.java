239f2491469823e39979f55329b0ecb2007b50cc
bobsharon <bobsharon@163.com>
2020-05-17 21:17:20 +0800

    upload demo source

---
+++ src/test/java/net/web/bobsharon/basewebquickstart/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package net.web.bobsharon.basewebquickstart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
